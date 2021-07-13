9414581975f121e0bffc903f3b12fd801c2250c8
radoneje <den.shevchenko@gmail.com>
2021-07-13 21:52:44 +0300

    ddd

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ var spawn = require('child_process').spawn;
 
 /* GET home page. */
 router.post('/restream', function(req, res, next) {
+try {
     var out = fs.openSync('/tmp/out.log', 'a');
-    var   err = fs.openSync('/tmp/out.log', 'a');
-  spawn('ffmpeg', ['-re','-i', 'rtmp://wowza02.onevent.online/live/stream'+req.body.roomid, "-c", "copy", "-f", "flv", req.body.rtmpURL],
-      {
-        detached: true,
-         stdio: [ 'ignore', out, err ]
-      }).unref();;
+    var err = fs.openSync('/tmp/out.log', 'a');
+    spawn('ffmpeg', ['-re', '-i', 'rtmp://wowza02.onevent.online/live/stream' + req.body.roomid, "-c", "copy", "-f", "flv", req.body.rtmpURL],
+        {
+            detached: true,
+            stdio: ['ignore', out, err]
+        }).unref();
+    ;
+}
+catch (e) {
+    console.warn(e)
+}
    res.json(0)
 });
 
